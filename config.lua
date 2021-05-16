a20579e66bd75d936d7be3831f7703c4a48f9a76
Darkonode <84207706+Darkonode@users.noreply.github.com>
2021-05-16 21:55:37 +0300

    Add files via upload

--- config.lua
+++ config.lua
@@ -20,7 +20,7 @@ local defaults = {
 }
 
 -------------------------------
--- Config function
+-- Event handlers
 -------------------------------
 function Config:Toggle()
 	local menu = ConfigWin or Config:CreateMenu()
@@ -32,47 +32,123 @@ function Config:GetThemeColor()
 	return c.r, c.g, c.b, c.hex
 end
 
-function Config:CreateMenu()
+local function Tab_OnClick(self)
+	local SelectedTabID = PanelTemplates_GetSelectedTab(self:GetParent())
+	if (SelectedTabID) then
+		if (SelectedTabID ~= self:GetID()) then
+			local TabName = "WLC_ConfigWinTab" .. SelectedTabID
+			_G[TabName].content:Hide()
+		end
+	end
+	
+	PanelTemplates_SetTab(self:GetParent(), self:GetID())
+	self.content:Show()
+end
 
-	ConfigWin = CreateFrame("Frame", "WLC_ConfigWin", UIParent, "BasicFrameTemplateWithInset")
-	ConfigWin:SetSize(600, 300)
-	ConfigWin:SetPoint("CENTER", UIParent, "CENTER")
+-------------------------------
+-- Config function
+-------------------------------
 
-	ConfigWin.title = ConfigWin:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
-	ConfigWin.title:SetPoint("CENTER", ConfigWin.TitleBg, "CENTER", 5, 0)
-	ConfigWin.title:SetText("Wislist Lootcouncil v0.1")
+local function SetTabs (frame, numTabs, ...)
+	frame.numTabs = numTabs
+	local tabs = {}
+	local frameName = frame:GetName()
+	
+	for i = 1, numTabs do
+		local tab = CreateFrame("Button", frameName.."Tab"..i, frame, "CharacterFrameTabButtonTemplate")
+		tab:SetID(i)
+		tab:SetText(select(i, ...))
+		tab:SetScript("OnClick", Tab_OnClick)
+		
+		tab.content = CreateFrame("Frame", nil, ConfigWin)
+		tab.content:SetSize(600, 300)
+		tab.content:SetPoint("CENTER", tab, "CENTER")
+		tab.content:Hide()
+		
+		table.insert(tabs, tab.content)
+		
+		if (i == 1) then
+			tab:SetPoint("TOPLEFT", ConfigWin, "BOTTOMLEFT", 5, 1)
+		else
+			tab:SetPoint("TOPLEFT", _G[frameName.."Tab"..(i-1)], "TOPRIGHT", -14, 0)
+		end
+		
+	end
+	
+	Tab_OnClick(_G[frameName.."Tab1"])
+	
+	return unpack(tabs)
+end
 
+local function PopulateTabs(tab, raid)
 	-----------------------------
 	-- Wishlist frame
 	-----------------------------
-	local WishlistFrame = CreateFrame("Frame", nil, ConfigWin, "InsetFrameTemplate")
+	tab.WishlistFrame = CreateFrame("Frame", nil, tab, "InsetFrameTemplate")
 
-	WishlistFrame:SetSize(200, 200)
-	WishlistFrame:SetPoint("RIGHT", ConfigWin, "RIGHT", -15, 0)
+	tab.WishlistFrame:SetSize(200, 230)
+	tab.WishlistFrame:SetPoint("TOPRIGHT", ConfigWin, "TOPRIGHT", -10, -45)
 
-	WishlistFrame.title = WishlistFrame:CreateFontString(nil, "OVERLAY")
-	WishlistFrame.title:SetFontObject("GameFontNormal")
-	WishlistFrame.title:SetPoint("CENTER", WishlistFrame, "TOP", 0, 10)
-	WishlistFrame.title:SetText("Raid X " .. "wishlist:")
+	tab.WishlistFrame.title = tab.WishlistFrame:CreateFontString(nil, "OVERLAY")
+	tab.WishlistFrame.title:SetFontObject("GameFontNormal")
+	tab.WishlistFrame.title:SetPoint("BOTTOMLEFT", tab.WishlistFrame, "TOPLEFT", 5, 3)
+	tab.WishlistFrame.title:SetText(raid .. " wishlist:")
 
 	-----------------------------
 	-- Wishlist frame buttons
 	-----------------------------
 	-- Clear button
-	ConfigWin.clearButton = CreateFrame("Button", nil, ConfigWin, "GameMenuButtonTemplate")
-	ConfigWin.clearButton:SetPoint("CENTER", WishlistFrame, "BOTTOMRIGHT", -35, -15)
-	ConfigWin.clearButton:SetSize(70, 25)
-	ConfigWin.clearButton:SetText("Clear")
-	ConfigWin.clearButton:SetNormalFontObject("GameFontNormalLarge")
-	ConfigWin.clearButton:SetHighlightFontObject("GameFontHighlightLarge")
+	tab.clearButton = CreateFrame("Button", nil, tab, "GameMenuButtonTemplate")
+	tab.clearButton:SetPoint("CENTER", tab.WishlistFrame, "BOTTOMRIGHT", -30, -11)
+	tab.clearButton:SetSize(60, 18)
+	tab.clearButton:SetText("Clear")
+	tab.clearButton:SetNormalFontObject("GameFontNormal")
+	tab.clearButton:SetHighlightFontObject("GameFontHighlight")
 
 	-- Delete button
-	ConfigWin.delButton = CreateFrame("Button", nil, ConfigWin, "GameMenuButtonTemplate")
-	ConfigWin.delButton:SetPoint("CENTER", WishlistFrame, "BOTTOMRIGHT", -110, -15)
-	ConfigWin.delButton:SetSize(70, 25)
-	ConfigWin.delButton:SetText("Delete")
-	ConfigWin.delButton:SetNormalFontObject("GameFontNormalLarge")
-	ConfigWin.delButton:SetHighlightFontObject("GameFontHighlightLarge")
+	tab.delButton = CreateFrame("Button", nil, tab.clearButton, "GameMenuButtonTemplate")
+	tab.delButton:SetPoint("TOPRIGHT", tab.clearButton, "TOPLEFT", -3, 0)
+	tab.delButton:SetSize(60, 18)
+	tab.delButton:SetText("Delete")
+	tab.delButton:SetNormalFontObject("GameFontNormal")
+	tab.delButton:SetHighlightFontObject("GameFontHighlight")
+	
+	-----------------------------
+	-- Item frame
+	-----------------------------
+	tab.ItemFrame = CreateFrame("Frame", nil, tab, "InsetFrameTemplate")
+	
+	tab.ItemFrame:SetSize(200, 212)
+	tab.ItemFrame:SetPoint("TOPRIGHT", tab.WishlistFrame, "TOPLEFT", -10, 0)
+
+	tab.ItemFrame.title = tab.ItemFrame:CreateFontString(nil, "OVERLAY")
+	tab.ItemFrame.title:SetFontObject("GameFontNormal")
+	tab.ItemFrame.title:SetPoint("BOTTOMLEFT", tab.ItemFrame, "TOPLEFT", 5, 3)
+	tab.ItemFrame.title:SetText(raid .. " drops:")
+	
+	-----------------------------
+	-- Item frame search bar
+	-----------------------------
+	tab.searchBar = CreateFrame("EditBox", nil, tab, "SearchBoxTemplate")
+	tab.searchBar:SetPoint("BOTTOM", tab.ItemFrame, "BOTTOM", 2, -16)
+	tab.searchBar:SetSize(195, 15)
+end
+
+function Config:CreateMenu()
+
+	ConfigWin = CreateFrame("Frame", "WLC_ConfigWin", UIParent, "BasicFrameTemplate")
+	ConfigWin:SetSize(600, 300)
+	ConfigWin:SetPoint("CENTER", UIParent, "CENTER")
+	ConfigWin.title = ConfigWin:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
+	ConfigWin.title:SetPoint("CENTER", ConfigWin.TitleBg, "CENTER", 0, 1)
+	ConfigWin.title:SetText("Wislist Lootcouncil v0.1")
+
+
+	local kara, gruul, magth = SetTabs(ConfigWin, 3, "Karazhan", "Gruul", "Magth")
+	
+	PopulateTabs(kara, "Karazhan")
+	PopulateTabs(gruul, "Gruul's Lair")
+	PopulateTabs(magth, "Magtheridon's Lair")
 
 	ConfigWin:Hide()
 	return ConfigWin
