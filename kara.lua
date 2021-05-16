4af747c4b3b409549e2cbfe2ffd3a6d4c0820ae8
Darkonode <84207706+Darkonode@users.noreply.github.com>
2021-05-17 01:12:54 +0300

    Add Karazhan item database.
    Added bosses: Servant Quarters, Attumen the Huntsman

---
+++ kara.lua
@@ -0,0 +1,133 @@
+-------------------------------
+-- Namespaces
+-------------------------------
+local _, namespace = ...
+namespace.Kara = {}
+
+local Kara = namespace.Kara
+
+local trash = {
+	RavagersBracers = {
+		id = 30687
+		name = "Ravager's Bracers"
+		type = "Plate"
+		slot = "Wrist"
+		source = Rare
+	},
+	RavagersCuffs = {
+		id = 30684
+		name = "Ravager's Cuffs"
+		type = "Cloth"
+		slot = "Wrist"
+		source = Rare
+	},
+	RavagersBands = {
+		id = 30686
+		name = "Ravager's Bands"
+		type = "Mail"
+		slot = "Wrist"
+		source = Rare
+	},
+	RavagersWristWraps = {
+		id = 30685
+		name = "Ravager's Wrist-Wraps"
+		type = "Leather"
+		slot = "Wrist"
+		source = Rare
+	}
+}
+
+local attumen = {
+	GlovesOfSaintlyBlessings = {
+		id = 28508
+		name = "Gloves of Saintly Blessings"
+		type = "Cloth"
+		slot = "Hands"
+		source = Attumen
+	},
+	BracersOfTheWhiteStag = {
+		id = 28453
+		name = "Bracers of the White Stag"
+		type = "Leather"
+		slot = "Wrist"
+		source = Attumen
+	},
+	SteelhawkCrossbow = {
+		id = 28504
+		name = "Steelhawk Crossbow"
+		type = "Crossbow"
+		slot = "Ranged"
+		source = Attumen
+	},
+	VambracersOfCourage = {
+		id = 28502
+		name = "Vambracers of Courage"
+		type = "Plate"
+		slot = "Wrist"
+		source = Attumen
+	},
+	WhirlwindBracers = {
+		id = 28503
+		name = "Whirlwind Bracers"
+		type = "Mail"
+		slot = "Wrist"
+		source = Attumen
+	},
+	GauntletsOfRenewedHope = {
+		id = 28505
+		name = "Gauntlets of Renewed Hope"
+		type = "Plate"
+		slot = "Hands"
+		source = Attumen
+	},
+	HandwrapsOfFlowingThought = {
+		id = 28507
+		name = "Handwraps of Flowing Thought"
+		type = "Cloth"
+		slot = "Hands"
+		source = Attumen
+	},
+	HarbingerBands = {
+		id = 28477
+		name = "Harbinger Bands"
+		type = "Cloth"
+		slot = "Wrist"
+		source = Attumen
+	},
+	SchematicStabilizedEterniumScope = {
+		id = 23809
+		name = "Schematic: Stabilized Eternium Scope"
+		type = "Schematic"
+		slot = nil
+		source = Attumen
+	},
+	GlovesOfDexterousManipulation = {
+		id = 28506
+		name = "Gloves of Dexterous Manipulation"
+		type = "Leather"
+		slot = "Hands"
+		source = Attumen
+	},
+	SpectralBandOfInnervation = {
+		id = 28510
+		name = "Spectral Band of Innervation"
+		type = "Ring"
+		slot = "Finger"
+		source = Attumen
+	},
+	StalkersWarBands = {
+		id = 28454
+		name = "Stalker's War Bands"
+		type = "Mail"
+		slot = "Wrist"
+		source = Attumen
+	},
+	WorgenClawNecklace = {
+		id = 28509
+		name = "Worgen Claw Necklace"
+		type = "Amulet"
+		slot = "Neck"
+		source = Attumen
+	},
+	
+}
